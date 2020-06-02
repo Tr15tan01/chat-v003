afc0a82786292f6802eefd6ea324f037dda01dd7
titevar <59051464+titevar@users.noreply.github.com>
2020-06-02 12:41:40 +0400

    changed index file

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", function(socket) {
+    socket.on("user_join", function(data) {
+        this.username = data;
+        socket.broadcast.emit("user_join", data);
+        //console.log(socket.id)
+       // console.log(socket.adapter.rooms[socket.id].length);
+        //console.log(Object.keys(io.of('/').connected).length)
+        console.log(Object.keys(io.of('/').server))
+    });
+
+    socket.on("chat_message", function(data) {
+        data.username = this.username;
+
+        socket.broadcast.emit("chat_message", data);
+    });
+
+    socket.on("disconnect", function(data) {
+        socket.broadcast.emit("user_leave", this.username);
+    });
+});
+
 http.listen(port, function() {
     console.log("Listening on *:" + port);
 })
